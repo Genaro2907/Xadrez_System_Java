401510517539d019da8e884a8d7687ff644d4650
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-07-26 19:33:54 -0300

    Implementando movimentos possiveis da torre

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,7 +8,6 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-
 public class Program {
 
 	public static void main(String[] args) {
@@ -17,29 +15,28 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		
-		try {
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		catch (ChessException e) {
-			System.out.println(e.getMessage());
-			sc.nextLine();
-		}
-		catch (InputMismatchException e) {
-			System.out.println(e.getMessage());
-			sc.nextLine();
-		}
-			
-		}
-
 	}
-
+}
